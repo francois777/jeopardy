2797275049218554487421af9acd42a1336c7a5f
Francois van der Hoven <francoisvanderhoven@gmail.com>
2017-10-01 04:02:32 +1000

    Fetch and render the clues for a category

--- src/components/Category.js
+++ src/components/Category.js
@@ -3,13 +3,30 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 class Category extends Component {
+  constructor() {
+    super();
+    this.state = { clues: [] };
+  }
+
+  componentDidMount() {
+    fetch(`http://jservice.io/api/clues?category=${this.props.category.id}`)
+    .then(response => response.json())
+    .then(json => this.setState({ clues: json }));
+  }
   render() {
     console.log('category props', this.props);
-    
+
     return (
       <div>
         <Link to='/' className='link-home'><h4>Home</h4></Link>
-        <h2>Category title</h2>
+        <h2>{this.props.category.title}</h2>
+        {
+          this.state.clues.map(clue => {
+            return (
+              <div key={clue.id}>{clue.question}</div>
+            )
+          })
+        }
       </div>
     )
   }
