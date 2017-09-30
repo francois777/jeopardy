d5f35c302e5bf6bdfa27de46e4d6275acb817c4d
Francois van der Hoven <francoisvanderhoven@gmail.com>
2017-09-30 19:11:45 +1000

    Add Category component with routes and styling.

--- src/components/Category.js
+++ src/components/Category.js
@@ -5,7 +5,7 @@ class Category extends Component {
   render() {
     return (
       <div>
-        <Link to='/'><h4>Home</h4></Link>
+        <Link to='/' className='link-home'><h4>Home</h4></Link>
         <h2>Category title</h2>
       </div>
     )
