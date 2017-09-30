d0aad1b88b1b9e6aaf286bfd24260acbab06bab9
Francois van der Hoven <francoisvanderhoven@gmail.com>
2017-09-30 18:41:25 +1000

    Prepare for routing.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import rootReducer from './reducers';
 import App from './components/App';
 
@@ -9,7 +10,11 @@ const store = createStore(rootReducer);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <Switch>
+        <Route exact={true} path='/' component={App} />
+      </Switch>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
