49248e35211ad71e5b7fb4129f161b74fa44e69f
Francois van der Hoven <francoisvanderhoven@gmail.com>
2017-10-01 04:38:31 +1000

    Reveal Clue answer when clicking on the Clue

--- src/components/Clue.js
+++ src/components/Clue.js
@@ -3,15 +3,22 @@ import React, { Component } from 'react';
 // import { Link } from 'react-router-dom';
 
 class Clue extends Component {
+  constructor() {
+    super();
+
+    this.state = { reveal: false }
+  }
+
   render(){
     const { answer, question, value } = this.props.clue;
+
     return(
-      <div>
+      <div className="clue" onClick={() => this.setState({reveal: true})}>
         <h4>{value || 'unknown'}</h4>
           <hr />
           <h5>{question}</h5>
           <hr />
-          <h5>{answer}</h5>
+          <h5 className={this.state.reveal ? 'text-revealed' : 'text-hidden'}>{answer}</h5>
       </div>
     );
   }
